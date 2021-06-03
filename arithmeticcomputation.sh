19fa213058733be26d1008cf6eed4060a4de96e4
sheetal-10 <sheetalp257@gmail.com>
2021-06-03 16:17:23 +0530

    readvalues from dictionary into array

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -1,4 +1,4 @@
-declare -A result
+declare -a resultArray
 read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
@@ -10,4 +10,8 @@ result[0]=$arithmeticOp1
 result[1]=$arithmeticOp2
 result[2]=$arithmeticOp3
 result[3]=$arithmeticOp4
-echo "result: ${result[@]}"
+for (( i=0; i<${#result[@]}; i++ ))
+do
+	resultArray[i]=${result[$i]}
+done
+echo "resultArray: ${resultArray[@]}"
