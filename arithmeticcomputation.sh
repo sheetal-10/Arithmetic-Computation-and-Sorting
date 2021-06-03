7bbf7545c6cce97402cba79f8b65d3dffc9ecdbd
sheetal-10 <sheetalp257@gmail.com>
2021-06-03 16:08:36 +0530

    a%b+c

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -1,5 +1,5 @@
 read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
-arithmeticOp3=$((c+(a/b))
-echo "ArithmeticOp3= $arithmeticOp3"
+arithmeticOp4=$(((a%b)+c
+echo "ArithmeticOp4= $arithmeticOp4"
