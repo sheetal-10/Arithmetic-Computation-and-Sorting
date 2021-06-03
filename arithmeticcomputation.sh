9d425747d47076cf0a7fe627868e7eb883da7d84
sheetal-10 <sheetalp257@gmail.com>
2021-06-03 16:13:38 +0530

    storing result in dictionary

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -1,5 +1,13 @@
+declare -A result
 read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
-arithmeticOp4=$(((a%b)+c
-echo "ArithmeticOp4= $arithmeticOp4"
+arithmeticOp1=$((a+(b*c)))
+arithmeticOp2=$(((a*b)+c))
+arithmeticOp3=$((c+(a/b)))
+arithmeticOp4=$(((a%b)+c))
+result[0]=$arithmeticOp1
+result[1]=$arithmeticOp2
+result[2]=$arithmeticOp3
+result[3]=$arithmeticOp4
+echo "result: ${result[@]}"
